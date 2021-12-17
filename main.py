2df17d7bbb82f704fcc75811701492ac6d1503c7
Pablo Díez Gómez <pablo.s278006@cesurformacion.com>
2021-12-17 18:54:28 +0100

    se añaden 2 lineas mas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
-    print("mi primera linea"g
-    )
+    print("mi primera linea de codigo")
+    print("mi segunda linea de codigo")
+    print("mi tercera linea de codigo")
